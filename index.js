b88c424e2a0141f63727269e344954f6a4eaebff
Thusitha Dharmaratne <thusithadh@gmail.com>
2018-12-29 02:01:48 +0530

    Added express js framework

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.write('<!DOCTYPE html>'+
-  '<html>'+
-  ' <head>'+
-  ' <meta charset="utf-8" />'+
-  ' <title>My Node.js page!</title>'+
-  ' </head>'+ 
-  ' <body>'+
-  ' <p>Here is a paragraph of <strong>HTML</strong>!</p>'+
-  ' </body>'+
-  '</html>');
-  res.end();
+
+var express = require('express');
+
+var app = express();
+
+
+  app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You are in reception. How can I help you?');
 });
-server.listen(PORT, () => {
+
+app.get('/basement', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\’re in the wine cellar. Those bottles are mine!');
+});
+
+app.get('/floor/1/bedroom', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hey, this is a private area!');
+});
+
+app.get('/floor/:floornum/bedroom', function(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('You\’re in the bedroom on floor n°' + req.params.variablename);
+});
+
+app.use(function(req, res, next){
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(404, 'Page cannot be found!');
+});
+
+app.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
 });
