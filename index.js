930edc8d57df88f1a2ce76108ce18eab86b37973
Thusitha Dharmaratne <thusithadh@gmail.com>
2018-12-29 00:55:20 +0530

    Added html code

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hi, Welcome to new world of NodeJs\n');
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write('<!DOCTYPE html>'+
+  '<html>'+
+  ' <head>'+
+  ' <meta charset="utf-8" />'+
+  ' <title>My Node.js page!</title>'+
+  ' </head>'+ 
+  ' <body>'+
+  ' <p>Here is a paragraph of <strong>HTML</strong>!</p>'+
+  ' </body>'+
+  '</html>');
+  res.end();
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
